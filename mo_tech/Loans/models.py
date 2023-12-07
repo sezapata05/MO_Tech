f59d0719712137f98718570f76c51304cf79f185
Seba Zapata <s.zapata05@outlook.com>
2023-12-06 20:53:32 -0500

    :building_construction: models generated

--- mo_tech/Loans/models.py
+++ mo_tech/Loans/models.py
@@ -1,3 +1,20 @@
+""" LoansModels module"""
 from django.db import models
+from Customers.models import CustumersModel
 
-# Create your models here.
+class LoansModel(models.Model):
+    """ LoansModel Class """
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    external_id = models.CharField(max_length=60)
+    status = models.SmallIntegerField(default=1)
+    amount = models.DecimalField(max_digits=12, decimal_places=2, default=0.0)
+    contract_version = models.SmallIntegerField(default=1)
+    maximum_payment_date = models.DateTimeField(auto_now=True)
+    taken_at = models.DateTimeField(auto_now=True)
+    custumers_id = models.ForeignKey(CustumersModel, on_delete=models.CASCADE)
+    outstanding = models.DecimalField(max_digits=12, decimal_places=2, default=0.0)
+
+    class Meta:
+        """ MetaClass """
+        db_table = 'loans_loan'
--- mo_tech/mo_tech/settings.py
+++ mo_tech/mo_tech/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'Customers',
+    'Loans',
+    'Payments'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+}
