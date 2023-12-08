aa61069aa1802f7179a537f8e8539acdae9ff16f
Seba Zapata <s.zapata05@outlook.com>
2023-12-08 09:01:07 -0500

    :construction: payments logic still in progress

--- mo_tech/Payments/views.py
+++ mo_tech/Payments/views.py
@@ -1,48 +1,39 @@
-""" Views module """
+""" Payments views module """
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
-from django.views import View
+from django.views.decorators.http import require_POST
 from rest_framework.parsers import JSONParser
-from Loans.models import LoansModel
-from .serializers import PaymentsSerializer
+from .services import PaymentService
+from .serializers import PaymentsModelSerializer
 
-@method_decorator(csrf_exempt, name='dispatch')
-class PaymentsView(View):
-    def post(self, request):
+@csrf_exempt
+@require_POST
+def create_payment(request):
+    """ POST METHOD """
+    try:
         data = JSONParser().parse(request)
-        serializer = PaymentsSerializer(data=data)
+        serializer = PaymentsModelSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
 
-        if serializer.is_valid():
-            loan_external_id = data['loan_external_id']
-            total_amount = data['total_amount']
+        payment = PaymentService.create_payment(
+            external_id=serializer.validated_data['external_id'],
+            customer_external_id=serializer.validated_data['custumers_id'].external_id,
+            loan_external_id=serializer.validated_data['paymentsdetailsmodel'][0]['loand_id'],
+            payment_date=serializer.validated_data['paid_at'],
+            total_amount=serializer.validated_data['total_amount'],
+            payment_amount=serializer.validated_data['paymentsdetailsmodel'][0]['amount']
+        )
 
-            loan = LoansModel.objects.get(external_id=loan_external_id)
-            if total_amount > loan.outstanding:
-                return JsonResponse({'error': 'El pago es superior a la deuda'}, status=400)
+        response_serializer = PaymentsModelSerializer(payment)
+        return JsonResponse(response_serializer.data)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=400)
 
-            payment = serializer.save()
-            PaymentsDetailsModel.objects.create(
-                amount=data['payment_amount'],
-                loand_id=loan,
-                payment_id=payment
-            )
-
-            loan.outstanding -= data['payment_amount']
-            loan.save()
-
-            return JsonResponse({'success': 'Pago creado exitosamente'}, status=201)
-        return JsonResponse(serializer.errors, status=400)
-
-    # def get(self, request):
-    #     customer_external_id = request.GET.get('customer_external_id')
-    #     loan_external_id = request.GET.get('loan_external_id')
-
-    #     payments = PaymentsModel.objects.filter(
-    #         custumers_id__external_id=customer_external_id,
-    #         paymentdetailsmodel__loand_id__external_id=loan_external_id
-    #     ).select_related('custumers_id').prefetch_related('paymentdetailsmodel_set')
-
-    #     serializer = PaymentsSerializer(payments, many=True)
-
-    #     return JsonResponse({'payments': serializer.data}, status=200)
+def get_payments_by_customer(request, customer_external_id):
+    """ GET METHOD """
+    try:
+        payments = PaymentService.get_payments_by_customer(customer_external_id)
+        response_serializer = PaymentsModelSerializer(payments, many=True)
+        return JsonResponse({'payments': response_serializer.data})
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=400)
